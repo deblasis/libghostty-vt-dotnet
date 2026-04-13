f896b35c72350064ffa6a6f47f392617f3b63648
Alessandro De Blasis <alex@deblasis.net>
2026-04-14 00:07:35 +0100

    feat: implement KittyGraphics image properties (#13)
    Adds managed wrappers for KittyGraphics image property accessors (ImageId, Format, Width, Height) via the native ghostty_kitty_graphics_image_get API.

--- tests/Ghostty.Vt.Tests/KittyGraphicsTests.cs
+++ tests/Ghostty.Vt.Tests/KittyGraphicsTests.cs
@@ -18,4 +18,34 @@ public class KittyGraphicsTests
     {
         using var term = new Terminal(80, 24);
     }
+
+    [Fact]
+    public void KittyImage_Format_DoesNotThrow()
+    {
+        using var term = new Terminal(80, 24);
+        var kitty = term.KittyGraphics;
+        var image = kitty.GetImage(1);
+        // Accessing Format should not throw NotImplementedException
+        var _ = image.Format;
+    }
+
+    [Fact]
+    public void KittyImage_Width_DoesNotThrow()
+    {
+        using var term = new Terminal(80, 24);
+        var kitty = term.KittyGraphics;
+        var image = kitty.GetImage(1);
+        // Accessing Width should not throw NotImplementedException
+        var _ = image.Width;
+    }
+
+    [Fact]
+    public void KittyImage_Height_DoesNotThrow()
+    {
+        using var term = new Terminal(80, 24);
+        var kitty = term.KittyGraphics;
+        var image = kitty.GetImage(1);
+        // Accessing Height should not throw NotImplementedException
+        var _ = image.Height;
+    }
 }
